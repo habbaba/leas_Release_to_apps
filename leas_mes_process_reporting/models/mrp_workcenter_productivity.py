c5ff6f155a18db83fe07bafffada591e8863ad10
habbaba <34061969+habbaba@users.noreply.github.com>
2024-09-11 16:56:37 +0300

    Update mrp_workcenter_productivity.py

--- leas_mes_process_reporting/models/mrp_workcenter_productivity.py
+++ leas_mes_process_reporting/models/mrp_workcenter_productivity.py
@@ -16,14 +16,12 @@ class MrpWorkcenterproductivity(models.Model):
     action_type = fields.Selection([('pause', 'Pause'), ('block', 'Block'), ('finish', 'Finish')])
     next_rec = fields.Many2one('mrp.workcenter.productivity', 'Next Record')
     workorder_state = fields.Selection(string='WorkOrder Status', related='workorder_id.state')
-    thirty_daily_efficiency = fields.Float(compute='_compute_thirty_daily_efficiency', store=True)
 
     @api.onchange('qty_started')
     def _onchange_qty_started(self):
         if self.env.context.get('_prevent_onchange') or not self.id.origin:
             return
         for line in self:
-
             old_value = line._origin.qty_started if line._origin else False
             new_value = line.qty_started
             diff = new_value - old_value
@@ -67,19 +65,19 @@ class MrpWorkcenterproductivity(models.Model):
     def _check_qty_completed(self):
         for line in self:
             if line.qty_completed > line.qty_started:
-                raise UserError(_("Not allowed to be greater than the started quantity."))
+                raise UserError(_(u"Not allowed to be greater than the started quantity."))
 
     @api.constrains('qty_started')
     def _check_qty_started(self):
         for line in self:
             if line.qty_started > line.workorder_id.qty_production:
-                raise UserError(_("Cannot Exceed WorkOrder Quantity."))
+                raise UserError(_(u"Cannot Exceed WorkOrder Quantity."))
             last_rec = line.next_rec
             while last_rec and last_rec.next_rec:
                 last_rec = last_rec.next_rec
             qty_comp = last_rec.qty_completed or line.qty_completed
             if line.qty_started < qty_comp:
-                raise UserError(_("Not allowed to be less than the completed quantity."))
+                raise UserError(_(u"Not allowed to be less than the completed quantity."))
 
     def get_processing_time_recs(self):
         self.ensure_one()
@@ -112,8 +110,9 @@ class MrpWorkcenterproductivity(models.Model):
         time_log = self
         temp_daily_efficiency = []
         for log in time_log:
-            if log.action_type == 'finish':
-                temp_daily_efficiency += log.calc_daily_efficiency()
+            daily_efficiency = log.calc_daily_efficiency()
+            if daily_efficiency:
+                temp_daily_efficiency += daily_efficiency  # Ensure the result is added only if it's valid
 
         date_to_duration = {}
         for item in temp_daily_efficiency:
@@ -122,6 +121,7 @@ class MrpWorkcenterproductivity(models.Model):
                 date_to_duration[date] += item['duration']
             else:
                 date_to_duration[date] = item['duration']
+
         daily_efficiency = {}
         for temp in temp_daily_efficiency:
             date = temp['date']
@@ -144,64 +144,71 @@ class MrpWorkcenterproductivity(models.Model):
     def calc_daily_efficiency(self):
         self.ensure_one()
         production_efficiencies = []
-        daily_avg_efficiencies = []
         output_quantity = self.qty_completed
         processing_times = self.get_processing_time_recs()
+
         if self.workorder_id:
             theoretical_time_per_product = self.workorder_id.duration_expected / self.workorder_id.qty_production
         else:
-            return production_efficiencies
+            return []  # Return an empty list if no work order is found
 
-        # Same logic for efficiency calculation
+        attendance_obj = self.env['resource.calendar.attendance'].sudo()
+        resource_calendar = self.sudo().workcenter_id.resource_calendar_id
+        daily_duration = []
+        for pro_time in processing_times:
+            start_datetime = pro_time.date_start
+            end_datetime = pro_time.date_end
+            if not end_datetime:
+                end_datetime = datetime.combine(start_datetime + timedelta(days=1), time(0, 0, 0))
+            current_datetime = start_datetime
+            total_production_time = timedelta(minutes=0)
+            daily_production_time = timedelta(minutes=0)
+            week_type = -1
+            if resource_calendar.two_weeks_calendar:
+                week_type = attendance_obj.get_week_type(start_datetime)
+            scheduling_plan = resource_calendar.attendance_ids.sorted(lambda t: t.hour_from)
 
-    def unlink(self):
-        if self.env.context.get('_force_unlink'):
-            return super().unlink()
-        for line in self:
-            rec = self.sudo().search([('next_rec', '=', line.id)], limit=1)
-            if line.action_type == 'finish' or line.next_rec:
-                raise UserError(_("Records cannot be deleted. You can achieve the same "
-                                  "result by modifying the timestamp or completing the quantity."))
-            elif rec:
-                rec.next_rec = False
-            else:
-                self.env['mrp.workorder'].sudo().browse(line.workorder_id.id).qty_operation_wip = \
-                    line.workorder_id.qty_operation_wip - line.qty_started
-            line.sudo().with_context(_force_unlink=True).unlink()
+            while current_datetime < end_datetime:
+                match_time = timedelta(minutes=1)
+                current_dayofweek = current_datetime.weekday()
+                current_plan = [plan for plan in scheduling_plan if
+                                plan.dayofweek == str(current_dayofweek) and plan.hour_from != plan.hour_to != 0 and (
+                                        week_type == -1 or plan.week_type == week_type)]
+                for plan in current_plan:
+                    scheduled_end = datetime.combine(current_datetime.date(), self.num_to_time(plan.hour_to))
+                    scheduled_start = datetime.combine(current_datetime.date(), self.num_to_time(plan.hour_from))
+                    if scheduled_end > current_datetime >= scheduled_start:
+                        end_time_to_use = min(scheduled_end, end_datetime)
+                        start_time_to_use = max(current_datetime, scheduled_start)
+                        match_time = end_time_to_use - start_time_to_use
+                        total_production_time += match_time
+                        daily_production_time += match_time
+                if (current_datetime + match_time).date() == (current_datetime + timedelta(days=1)).date() or \
+                        (current_datetime + match_time) >= end_datetime:
+                    if daily_production_time:
+                        daily_duration.append({"date": current_datetime.date(),
+                                               "duration": daily_production_time.total_seconds() / 60})
+                    elif (end_datetime - start_datetime).days == 0:
+                        daily_duration.append({"date": current_datetime.date(),
+                                               "duration": (end_datetime - start_datetime).total_seconds() / 60})
+                    daily_production_time = timedelta(minutes=0)
+                current_datetime += match_time
 
-    @staticmethod
-    def num_to_time(num):
-        hours_from = int(num)
-        minutes_from = int((hours_from - num) * 60)
-        return time(hours_from, minutes_from, 0)
+        total_duration = sum([item["duration"] for item in daily_duration])
+        if total_duration:
+            total_efficiency = (output_quantity * theoretical_time_per_product) / total_duration
+            for entry in daily_duration:
+                production_efficiencies.append({
+                    "date": entry['date'],
+                    "duration": entry['duration'] / 60,  # hours
+                    "efficiency": total_efficiency
+                })
 
-    @api.depends('workorder_id.qty_production', 'next_rec.qty_completed')
-    def _compute_thirty_daily_efficiency(self):
-        """
-        Compute method for `thirty_daily_efficiency` that calculates efficiency
-        over the past 30 days or based on completed and production quantities.
-        """
-        for record in self:
-            try:
-                if not record.workorder_id or not record.workorder_id.qty_production:
-                    record.thirty_daily_efficiency = 0.0
-                    continue
+        return production_efficiencies
 
-                total_efficiency = 0.0
-                total_qty = 0.0
-
-                # Fetch workorders and compute efficiency
-                if record.qty_completed and record.qty_started:
-                    efficiency = (record.qty_completed / record.qty_started) * 100
-                    total_efficiency += efficiency
-                    total_qty += 1
-
-                # Compute the final efficiency based on total_qty
-                if total_qty > 0:
-                    record.thirty_daily_efficiency = total_efficiency / total_qty
-                else:
-                    record.thirty_daily_efficiency = 0.0
-
-            except Exception as e:
-                _logger.error(f"Error computing thirty_daily_efficiency for Record {record.id}: {e}")
-                record.thirty_daily_efficiency = 0.0
+    def num_to_time(self, num):
+        if num == 0:
+            return time(0, 0, 0)
+        hours = int(num / 100)
+        minutes = int(num % 100)
+        return time(hours, minutes, 0)
